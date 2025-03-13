5bb411ba4d9d073c4af47f406d6d45c63906d88b
OEncouracado <tryironcl4d@gmail.com>
2025-03-13 17:59:38 -0300

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,30 @@
 "use client";
 
-import { Box, Container } from "@mui/material";
-
+import { Box, Card, CardContent, Container } from "@mui/material";
+const styles = `
+.teste {
+  background-image: url('https://genyo.com.br/wp-content/uploads/2023/06/ferramentas-para-rh-e-dp.webp');
+  background-color: red;
+  width: 100px;
+  height: 100px;
+}
+`;
 export default function Home() {
   return (
-    <Container>
-      <Box>
-        <h1>Home 1</h1>
+    <Container maxWidth="lg">
+      <Box sx={{ my: 4 }}>
+        <Card>
+          <CardContent>
+            <h1>Home 1</h1>
+          </CardContent>
+        </Card>
+      </Box>
+      <Box sx={{ my: 4 }}>
+        <Card>
+          <CardContent>
+            <div className={styles}>TEste</div>
+          </CardContent>
+        </Card>
       </Box>
     </Container>
   );
